ad1a0d45d2ff5ae49d0523a654241127ded30a94
Abhijit Brahme <abhijitbrahme@Abhijits-MacBook-Pro.local>
2024-06-19 17:36:38 -0700

    page one of dashboard

---
+++ app.py
@@ -0,0 +1,110 @@
+import pickle
+import jax.numpy as jnp
+import numpy as np
+import pandas as pd
+from shiny import reactive, render, req, App
+from shinywidgets import render_plotly
+from shiny.express import input, ui
+from visualization.visualization import plot_correlation_dendrogram, plot_mcmc_diagnostics, plot_posterior_predictive_career_trajectory, plot_scatter
+from data.data_utils import create_fda_data
+
+
+#### LOAD EVERYTHING 
+data = pd.read_csv("data/player_data.csv").query(" age <= 38 ")
+names = data.groupby("id")["name"].first().values
+
+metric_output = (["gaussian"] * 3) + (["poisson"] * 9) + (["binomial"] * 3)
+
+metrics = ["log_min", "obpm","dbpm","blk","stl","ast","dreb","oreb","tov","fta","fg2a","fg3a","ftm","fg2m","fg3m"]
+exposure_list = ["simple_exposure"] + (["minutes"] * 11) + ["fta","fg2a","fg3a"]
+
+
+data["log_min"] = np.log(data["minutes"])
+data["simple_exposure"] = 1 
+_ , outputs, _ = create_fda_data(data, basis_dims=3, metric_output=metric_output, 
+                                     metrics = metrics
+, exposure_list =  exposure_list)
+
+with open("model_output/fixed_nba_rflvm.pkl", "rb") as f:
+    results = pickle.load(f)
+f.close()
+
+W = results["W"]
+with open("model_output/exponential_cp.pkl", "rb") as f:
+    results_embedding = pickle.load(f)
+f.close()
+
+X = results_embedding["U_auto_loc"]
+U, _, _ = np.linalg.svd(X, full_matrices=False)
+L       = np.linalg.cholesky(np.cov(U.T) + 1e-6 * np.eye(3)).T
+aligned_X  = np.linalg.solve(L, U.T).T
+X_rflvm_aligned = aligned_X / np.std(X, axis=0)
+
+
+
+
+parameters = results.keys()
+
+agg_dict = {"obpm":"mean", "dbpm":"mean", "bpm":"mean", 
+        "minutes":"sum", "dreb": "sum", "fta":"sum", "ftm":"sum", "oreb":"sum",
+        "ast":"sum", "tov":"sum", "fg2m":"sum", "fg3m":"sum", "fg3a":"sum", "fg2a":"sum", "blk":"sum", "stl":"sum"}
+data["total_minutes"] = data["median_minutes_per_game"] * data["games"] 
+agged_data = data.groupby("id").agg(agg_dict).reset_index()
+agged_data["ft_pct"] = agged_data["ftm"] / agged_data["fta"]
+agged_data["fg2_pct"] = agged_data["fg2m"] / agged_data["fg2a"]
+agged_data["fg3_pct"] = agged_data["fg3m"] / agged_data["fg3a"]
+agged_data["dreb_rate"] = 36.0 * agged_data["dreb"] / agged_data["minutes"]
+agged_data["oreb_rate"] = 36.0 * agged_data["oreb"] / agged_data["minutes"]
+agged_data["ast_rate"] = 36.0 * agged_data["ast"] / agged_data["minutes"]
+agged_data["tov_rate"] = 36.0 * agged_data["tov"] / agged_data["minutes"]
+agged_data["blk_rate"] = 36.0 * agged_data["blk"] / agged_data["minutes"]
+agged_data["stl_rate"] = 36.0 * agged_data["stl"] / agged_data["minutes"]
+agged_data["ft_rate"] = 36.0 * agged_data["fta"] / agged_data["minutes"]
+agged_data["fg2_rate"] = 36.0 * agged_data["fg2a"] / agged_data["minutes"]
+agged_data["fg3_rate"] = 36.0 * agged_data["fg3a"] / agged_data["minutes"]
+agged_data.fillna(0, inplace=True)
+df = pd.concat([pd.DataFrame(X_rflvm_aligned, columns=[f"dim{i+1}" for i in range(3)]), agged_data], axis = 1)
+df["player_name"] = names
+
+
+
+
+agged_metrics = ["obpm","minutes","dbpm"] + [col for col in df.columns if ("rate" in col) or ("pct" in col)]
+
+ui.page_opts(title="NBA Career Trajectories")
+
+with ui.nav_panel("Player Embeddings & Trajectories"):
+    with ui.layout_column_wrap():
+    
+        ui.input_select(id="metric", label = "Select a metric", choices = {index : name for index, name in enumerate(agged_metrics)})
+        ui.input_select(id="player", label = "Select a player", choices = {index : name for index, name in enumerate(names)})
+    with ui.layout_column_wrap():
+        @render_plotly
+        def plot_latent_space():
+            return plot_scatter(df, agged_metrics[int(input.metric())], "Latent Embedding" )
+
+        
+        @render_plotly
+        def player_trajectory():
+            player_index = 0 if input.player() is None else int(input.player())
+            player_name = names[player_index]
+            wTx = np.einsum("r,ijmr -> ijm", X_rflvm_aligned[player_index], W)
+            phi = np.concatenate([np.cos(wTx), np.sin(wTx)], -1) * (1/ np.sqrt(100))
+            mu = np.einsum("ijk,ijmkt -> ijmt", phi, results["beta"])
+            return plot_posterior_predictive_career_trajectory(player_name=player_name,
+                                                            player_index=player_index,
+                                                            metrics=metrics, 
+                                                            metric_outputs=metric_output,
+                                                            posterior_mean_samples=mu,
+                                                            observations=jnp.stack([output["output_data"] for output in outputs], axis = 1), 
+                                                            exposures = jnp.stack([outputs[i]["exposure_data"] for i in range(len(metrics))],axis=0),
+                                                            posterior_variance_samples=jnp.stack([results["sigma_log_min"], results["sigma_obpm"], results["sigma_dbpm"]], axis = 0))
+
+
+
+
+# with ui.nav_panel("Player Correlations"):
+#     @render_plotly
+#     def plot_latent_space_dendrogram():
+#         return plot_correlation_dendrogram(X_rflvm_aligned, labels = names, title = "Embedding Correlation")
+
--- visualization/visualization.py
+++ visualization/visualization.py
@@ -1,47 +1,165 @@
-import arviz as az 
-import jax
-import jax.numpy as jnp
+import matplotlib.pyplot as plt
+import plotly.figure_factory as ff
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+import plotly.express as px
+from scipy.spatial.distance import pdist, squareform
 import numpy as np
-from numpyro.distributions import Normal, Poisson, Binomial
+import arviz as az
+from model.inference_utils import create_metric_trajectory
 
-
-def plot_metric_trajectory(player_index, posterior_mean_samples, observations, exposures, metric_outputs: list[str], posterior_variance_samples = None):
-    key = jax.random.key(0)
-    gaussian_index = 0
-    posteriors = []
-    obs_normalized = []
+def plot_posterior_predictive_career_trajectory(player_name, player_index, metrics: list[str], metric_outputs: list[str], posterior_mean_samples, observations, exposures, posterior_variance_samples):
+    """
+    plots the posterior predictive career trajectory 
+    """
+    fig = make_subplots(rows = 3, cols=5,  subplot_titles=metrics)
     
-    for metric_index, metric_output in enumerate(metric_outputs):
-        exposure = exposures[metric_index, ...]
-        obs = observations[player_index, metric_index, :]
-        post = posterior_mean_samples[..., metric_index, player_index, :]
-        inds = jnp.where(jnp.isnan(exposure))
-        exposure = exposure.at[inds].set(np.take(np.nanmean(exposure, axis = 0), inds[1]))
+    observation_dict, posterior_dict = create_metric_trajectory(posterior_mean_samples, player_index,  observations, exposures, 
+                                                                metric_outputs=metric_outputs, posterior_variance_samples=posterior_variance_samples)
 
-        if metric_output == "gaussian":
-            scale = jnp.einsum("cs,t -> cst", posterior_variance_samples[gaussian_index],  1.0 / exposure[player_index, :])
-            dist = Normal()
-            posterior_predictions = (dist.sample(key = key, sample_shape=post.shape) * scale + post)
-            obs_normal = obs
-            gaussian_index += 1
-        elif metric_output == "poisson":
-            dist = Poisson(rate = jnp.exp(post + exposures[player_index, :]))
-            posterior_predictions = 36.0 * (dist.sample(key = key) / jnp.exp(exposure[player_index, :])) ### per 36 min statistics
-            obs_normal = 36.0 * (obs / jnp.exp(exposure[player_index, :]))
-        elif metric_output == "binomial":
-            dist = Binomial(total_count = jnp.round(exposure[player_index, :], decimals=0), logits = post)
-            posterior_predictions = dist.sample(key = key) / jnp.round(exposure[player_index, :], decimals=0) ### per shot
-            obs_normal = obs / jnp.round(exposure[player_index, :], decimals=0) ### per shot
+ 
+    obs = observation_dict["y"]
+    posterior = posterior_dict["y"]
+    x = list(range(18,39))
+    for index, metric in enumerate(metrics):
+        row = int(np.floor(index / 5)) + 1 
+        col = (index % 5) + 1
+        metric_type = metric_outputs[index]
+        metric = metric.upper()
+        if metric_type == "poisson":
+            metric += " per 36 min"
+        fig.add_trace(go.Scatter(x = x, y = obs[..., index], mode = "lines", 
+                                 name = "Observed", line_color = "blue", showlegend=False), row = row, col=col)
+        fig.add_trace(go.Scatter(x = x, y = posterior[..., index].mean(axis = (0,1)), mode = "lines", name = "Posterior Mean", line_color = "red", showlegend=False), row = row, col = col)
+        lb = np.percentile(posterior[..., index], q = 5, axis = (0,1))
+        ub = np.percentile(posterior[..., index], q = 95, axis = (0,1))
+        fig.add_trace(go.Scatter(
+        name='Upper Bound',
+        x=x,
+        y=ub,
+        mode='lines',
+        marker=dict(color="#444"),
+        line=dict(width=0),
+        showlegend=False),row = row, col=col)
+
+        fig.add_trace(go.Scatter(
+            name='Lower Bound',
+            x=x,
+            y=lb,
+            marker=dict(color="#444"),
+            line=dict(width=0),
+            mode='lines',
+            fillcolor='rgba(68, 68, 68, 0.3)',
+            fill='tonexty',
+            showlegend=False,
+        ), row = row, col=col)
+
+    fig.update_layout({'width':650, 'height': 650,
+                            'showlegend':False, 'hovermode': 'closest',
+                            "title": player_name})
     
-        posteriors.append(posterior_predictions)
-        obs_normalized.append(obs_normal)
+    return fig
 
 
-    obs_data = {"y": jnp.stack(obs_normalized, axis = -1)}  ### has shape (time, metrics)
-    print(obs_data["y"].shape)
-    posterior_predictive = {"y": jnp.stack(posteriors, axis = -1)}  ### has shape (chains, draws, time, metrics)
-    print(posterior_predictive["y"].shape)
-    idata = az.from_dict(observed_data=obs_data, posterior_predictive=posterior_predictive,
-                         dims={"y": ["age", "metric"]},
-                         coords={"age": range(21), "metric": range(len(metric_outputs))})
-    az.plot_ts(idata, y = "y", plot_dim="age")
+def plot_mcmc_diagnostics(inference_data, variable_name, coords = None):
+    trace_plot = az.plot_trace(inference_data, var_names=variable_name, coords=coords)
+    post_plot = az.plot_posterior(inference_data, var_names = variable_name, coords = coords)
+    autocorr_plot = az.plot_autocorr(inference_data, var_names = variable_name, coords=coords)
+    summary = az.summary(inference_data, var_names = variable_name, coords=coords)
+    return trace_plot, post_plot, autocorr_plot, summary
+
+def plot_correlation_dendrogram(X, labels, title = ""):
+    # Initialize figure by creating upper dendrogram
+    fig = ff.create_dendrogram(X, orientation='bottom', labels=labels, )
+    for i in range(len(fig['data'])):
+        fig['data'][i]['yaxis'] = 'y2'
+
+    # Create Side Dendrogram
+    dendro_side = ff.create_dendrogram(X, orientation='right')
+    for i in range(len(dendro_side['data'])):
+        dendro_side['data'][i]['xaxis'] = 'x2'
+
+    # Add Side Dendrogram Data to Figure
+    for data in dendro_side['data']:
+        fig.add_trace(data)
+
+    # Create Heatmap
+    dendro_leaves = dendro_side['layout']['yaxis']['ticktext']
+    dendro_leaves = list(map(int, dendro_leaves))
+    data_dist = pdist(X)
+    heat_data = squareform(data_dist)
+    heat_data = heat_data[dendro_leaves,:]
+    heat_data = heat_data[:,dendro_leaves]
+
+    heatmap = [
+        go.Heatmap(
+            x = dendro_leaves,
+            y = dendro_leaves,
+            z = heat_data,
+            colorscale = 'Blues'
+        )
+    ]
+
+    heatmap[0]['x'] = fig['layout']['xaxis']['tickvals']
+    heatmap[0]['y'] = dendro_side['layout']['yaxis']['tickvals']
+
+    # Add Heatmap Data to Figure
+    for data in heatmap:
+        fig.add_trace(data)
+
+    # Edit Layout
+    fig.update_layout({'width':800, 'height':800,
+                            'showlegend':False, 'hovermode': 'closest',
+                            "title": title})
+    # Edit xaxis
+    fig.update_layout(xaxis={'domain': [.15, 1],
+                                    'mirror': False,
+                                    'showgrid': False,
+                                    'showline': False,
+                                    'zeroline': False,
+                                    'ticks':""})
+    # Edit xaxis2
+    fig.update_layout(xaxis2={'domain': [0, .15],
+                                    'mirror': False,
+                                    'showgrid': False,
+                                    'showline': False,
+                                    'zeroline': False,
+                                    'showticklabels': False,
+                                    'ticks':""})
+
+    # Edit yaxis
+    fig.update_layout(yaxis={'domain': [0, .85],
+                                    'mirror': False,
+                                    'showgrid': False,
+                                    'showline': False,
+                                    'zeroline': False,
+                                    'showticklabels': False,
+                                    'ticks': ""
+                            })
+    # Edit yaxis2
+    fig.update_layout(yaxis2={'domain':[.825, .975],
+                                    'mirror': False,
+                                    'showgrid': False,
+                                    'showline': False,
+                                    'zeroline': False,
+                                    'showticklabels': False,
+                                    'ticks':""})
+    
+    
+    return fig 
+
+def plot_scatter(df, color_col: str, title= ""):
+    fig = px.scatter_3d(df, x = "dim1", y = "dim2", z = "dim3", color = color_col, size = "minutes", opacity = .7, title=title,
+                        hover_data = ["player_name", "minutes"] + [color_col])
+    return fig
+
+
+
+
+        
+
+
+
+
+            
+
