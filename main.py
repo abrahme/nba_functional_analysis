this model doesn't introduce variability in the peak age, but has variance on c_max. the peak age are derived from estimates of metric wide peak ages and used as fixed inputs to the model. as you can see, the behaviour after these fixed ages (Around 25) flatlines if the player hasn't already peaked by then

main.py
@@ -206,7 +206,7 @@ if __name__ == "__main__":
                 if "max" in model_name:
                     model_args.update({"offsets": offset_dict})
             if svi_inference:
-                samples = model.run_svi_inference(num_steps=1000, model_args=model_args, initial_values=initial_params)
+                samples = model.run_svi_inference(num_steps=100000, model_args=model_args, initial_values=initial_params)
             elif not neural_parametrization:
                 samples, extra_fields = model.run_inference(num_chains=4, num_samples=2000, num_warmup=1000, vectorized=vectorized, model_args=model_args, initial_values=initial_params)
             else:
@@ -238,7 +238,7 @@ if __name__ == "__main__":
         shifted_x_time = hsgp_params["shifted_x_time"]
         spd = jnp.sqrt(diag_spectral_density(1, alpha_time, ls_deriv, L_time, M_time))
         weights = samples["beta__loc"]
-        weights = weights * spd * .0000001
+        weights = weights * spd * .0001
         W = samples["W__loc"]
         lengthscale = samples["lengthscale__loc"][None]
 
@@ -315,7 +315,7 @@ if __name__ == "__main__":
         X /= jnp.std(X, keepdims = True, axis = 0)
         wTx = jnp.einsum("nr, mr -> nm", X, W * jnp.sqrt(lengthscale))
         psi_x = jnp.concatenate([jnp.cos(wTx), jnp.sin(wTx)], axis = -1) * (1/ jnp.sqrt(100))
-        intercept = make_psi_gamma(psi_x, c_max)  * .00005 + offset_dict["c_max"]
+        intercept = make_psi_gamma(psi_x, c_max)   + offset_dict["c_max"]
         t_max = offset_dict["t_max"]
         phi_tmax = make_convex_phi(t_max, L_time, M_time)
         phi_prime_tmax = make_convex_phi_prime(t_max, L_time, M_time)

model/models.py
@@ -534,11 +534,11 @@ class ConvexMaxTVRFLVM(TVRFLVM):
         t_max = offsets["t_max"]
         psi_x = jnp.concatenate([jnp.cos(wTx), jnp.sin(wTx)], axis = -1) * (1/ jnp.sqrt(self.m))                
         ls_deriv = self.prior["lengthscale_deriv"] if not isinstance(self.prior["lengthscale_deriv"], Distribution) else sample("lengthscale_deriv", self.prior["lengthscale_deriv"], sample_shape=(num_metrics,))
-        intercept = make_psi_gamma(psi_x, self.prior["c_max"] if not isinstance(self.prior["c_max"], Distribution) else sample("c_max", self.prior["c_max"] , sample_shape=(2 * self.m, num_metrics))) * .00005 + offsets["c_max"]
+        intercept = make_psi_gamma(psi_x, self.prior["c_max"] if not isinstance(self.prior["c_max"], Distribution) else sample("c_max", self.prior["c_max"] , sample_shape=(2 * self.m, num_metrics)))  + offsets["c_max"]
         alpha_time = self.prior["alpha"] if not isinstance(self.prior["alpha"], Distribution) else sample("alpha", self.prior["alpha"], sample_shape=(num_metrics, ))
         spd = jnp.sqrt(diag_spectral_density(1, alpha_time, ls_deriv, L_time, M_time))
         weights = self.prior["beta"] if not isinstance(self.prior["beta"], Distribution) else sample("beta", self.prior["beta"], sample_shape=(self.m * 2, M_time, num_metrics))
-        weights = weights * spd * .0000001
+        weights = weights * spd * .0001
         phi_tmax = make_convex_phi(t_max, L_time, M_time)
         phi_prime_tmax = make_convex_phi_prime(t_max, L_time, M_time)
         gamma_phi_gamma_x_tmax = jnp.einsum("nm, mdk, ktdz, jzk, nj -> knt", psi_x, weights, phi_tmax[..., None, :, :] - phi_time[None], weights, psi_x)
@@ -570,9 +570,9 @@ class ConvexMaxTVRFLVM(TVRFLVM):
         return super().run_inference(num_warmup, num_samples, num_chains, vectorized, model_args, initial_values)
     
     def run_svi_inference(self, num_steps, guide_kwargs: dict = {}, model_args: dict = {}, initial_values:dict = {}):
-        guide = AutoDelta(self.model_fn, prefix="", init_loc_fn = init_to_uniform, **guide_kwargs)
+        guide = AutoDelta(self.model_fn, prefix="", init_loc_fn = init_to_median, **guide_kwargs)
         print("Setup guide")
-        svi = SVI(self.model_fn, guide, optim=adam(.0000003), loss=Trace_ELBO(num_particles=1)
+        svi = SVI(self.model_fn, guide, optim=adam(.00003), loss=Trace_ELBO(num_particles=1)
                   )
         print("Setup SVI")
         result = svi.run(jax.random.PRNGKey(0),